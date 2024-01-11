07786eb9e56a0b2182407cab062e6e578eed531b
siepra <wiktor.siepra@gmail.com>
2024-01-11 14:02:44 +0100

    feat: attach remaining iOS methods

--- ios/E2ee.m
+++ ios/E2ee.m
@@ -1,6 +1,23 @@
 #import <React/RCTBridgeModule.h>
 
 @interface RCT_EXTERN_MODULE(E2ee, NSObject)
+
 RCT_EXTERN_METHOD(generateKeyPair:(RCTPromiseResolveBlock)resolve
-                  reject:(RCTPromiseRejectBlock)reject)
+                           reject:(RCTPromiseRejectBlock)reject
+                  )
+
+RCT_EXTERN_METHOD(getOwnPublicKey:(RCTPromiseResolveBlock)resolve
+                           reject:(RCTPromiseRejectBlock)reject
+                  )
+
+RCT_EXTERN_METHOD(encryptMessage:(NSString)message
+                       publicKey:(NSString)publicKey
+                         resolve:(RCTPromiseResolveBlock)resolve
+                          reject:(RCTPromiseRejectBlock)reject
+                  )
+
+RCT_EXTERN_METHOD(decryptMessage:(NSString)message
+                         resolve:(RCTPromiseResolveBlock)resolve
+                          reject:(RCTPromiseRejectBlock)reject
+                  )
 @end
--- ios/E2ee.swift
+++ ios/E2ee.swift
@@ -1,3 +1,6 @@
+import UIKit
+import SwiftyRSA
+
 import Foundation
 
 public typealias RCTPromiseResolveBlock = (Any?) -> Void
@@ -6,11 +9,50 @@ public typealias RCTPromiseRejectBlock = (String?, String?, NSError?) -> Void
 @objc(E2ee)
 class E2ee: NSObject {
 
-    let RSAKeyManager = RSAKeyManager.shared
+    let _RSAKeyManager = RSAKeyManager.shared
 
     @objc(generateKeyPair:reject:)
     func generateKeyPair(_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-        let publicKey = RSAKeyManager.generateKeyPair()
+        let publicKey = _RSAKeyManager.generateKeyPair()
         resolve(publicKey)
+    }
+    
+    @objc(getOwnPublicKey:reject:)
+    func getOwnPublicKey(_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
+        let publicKey = _RSAKeyManager.getMyPublicKeyString()
+        resolve(publicKey)
+    }
+    
+    @objc(encryptMessage:publicKey:resolve:reject:)
+    func encryptMessage(_ message: String, publicKey: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
+        do {
+            let clear = try ClearMessage(string: message, using: .utf8)
+            
+            let publicKeyObject = _RSAKeyManager.getPublicKey(pemEncoded: publicKey)!
+            
+            let encryptedMessage = try clear.encrypted(with: publicKeyObject, padding: .PKCS1)
+            let encryptedMessageString = encryptedMessage.base64String
+            
+            resolve(encryptedMessageString)
+        } catch let error {
+            //Log error
+            reject("MESSAGE ENCRYPTION ERROR", "Cannot encrypt message", nil)
+        }
+    }
+    
+    @objc(decryptMessage:resolve:reject:)
+    func decryptMessage(_ message: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
+        do {
+            let privateKey = RSAKeyManager.shared.getMyPrivateKey()!
+            
+            let encrypted = try EncryptedMessage(base64Encoded: message)
+            let clear = try encrypted.decrypted(with: privateKey, padding: .PKCS1)
+            let decrypted = try clear.string(encoding: .utf8)
+            
+            resolve(decrypted)
+        } catch let error {
+            //Log error
+            reject("MESSAGE DECRYPTION ERROR", "Cannot decrypt message", nil)
+        }
     }
 }
